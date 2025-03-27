1098b943258e16f5a8c55938c942ecf76b9ce71d
hanwei <qqpp0504@gmail.com>
2025-03-27 15:18:16 +0800

    Start creat my website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navigation from "./components/Navigation.jsx";
+
+function App() {
+  return (
+    <div className="montserrat">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+export default function Navigation() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex gap-5 text-xl justify-end">
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
